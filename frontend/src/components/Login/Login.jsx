8c3c98f28f20a62ec29fd5ec04bdcb81e02cfd8f
LAPTOP-PPVTJ3FP\ADMIN <leluuquocdat9@gmail.com>
2023-08-09 14:06:24 +0700

    concat

--- frontend/src/components/Login/Login.jsx
+++ frontend/src/components/Login/Login.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
-
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 const Login = () => {
   const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [visible, setVisible] = useState(false);
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
@@ -39,18 +41,31 @@ const Login = () => {
                 >
                   Mật khẩu
                 </label>
-                <div className="mt-1">
+                <div className="mt-1 relative">
                   <input
                     type="password"
                     name="password"
-                    autoComplete="email"
+                    autoComplete="current-password"
                     placeholder="Nhập mật khẩu của bạn"
                     required
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                     className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadown-sm placeholder-gray-400 focus:outline-none 
                     focus:ring-blue-500, focus:border-blue-500 sm:text-sm"
                   />
+                  {visible ? (
+                    <AiOutlineEye
+                      className="absolute top-2 right-2  cursor-pointer"
+                      size={25}
+                      onClick={() => setVisible(false)}
+                    />
+                  ) : (
+                    <AiOutlineEyeInvisible
+                      className="absolute top-2 right-2  cursor-pointer"
+                      size={25}
+                      onClick={() => setVisible(true)}
+                    />
+                  )}
                 </div>
               </div>
             </form>
